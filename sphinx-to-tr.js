ed3bb78e6581a519c5b856418743f0e81910571c
Eric Prud'hommeaux <eric+github@w3.org>
2021-06-26 09:53:06 +0200

    + class SphinxToTr

--- sphinx-to-tr.js
+++ sphinx-to-tr.js
@@ -8,8 +8,86 @@ const PROTOCOL = 'http:'
 const HOST = 'www.w3.org'
 const PORT = 80
 const WEBROOT = '/TR/'
-const FSROOT = '../../webassembly/spec/core/'
 
+// Working class to translate Sphinx docs to W3C TR/ format
+class SphinxToTr {
+  constructor (path) {
+    const parsed = Path.parse(path)
+    this.relDir = parsed.dir
+    this.page = parsed.name + parsed.ext
+  }
+
+  async index () {
+    try {
+      const { dom, document, url } = await this.loadPage(this.page, 100)
+      const dir = new URL('..', url).href
+      if (!('$' in dom.window))
+        throw new Error('jQuery failed to load')
+      const find =
+            // (selectors, from) => (from ? dom.window.$(from).find(selectors) : dom.window.$(selectors)).get() // jQuery
+            (selectors, from) => [...(from || document).querySelectorAll(selectors)] // DOM
+      const urlStrToElements =
+            find('a')
+            .map( (elt) => [SphinxToTr.noHash(elt), elt] )
+            .filter( ([urlStr, elt]) => [urlStr.startsWith(dir), elt] )
+            .map( ([urlStr, elt]) => [urlStr.substr(dir.length), elt] )
+            .reduce( (acc, [urlStr, elt]) => acc.set(urlStr, elt), new ArrayMap())
+      urlStrToElements.delete('')
+      console.log(`${this.page} has ${urlStrToElements.total} references to ${urlStrToElements.size} descendants of ${dir}`)
+      
+      console.log(('li', find('[role=navigation] ul')[0]))
+      // document.documentElement.outerHTML
+    } catch (e) {
+      console.warn('caught:', e)
+    }
+  }
+
+  async loadPage (page, timeout) {
+    const path = Path.join(__dirname, this.relDir, page) // paths relative to repo root
+    let url = new URL('file://' + path)
+    let dom = getDom(page)
+    // dom.window.navigator.appVersion = dom.window.navigator.userAgent
+    let timer = null;
+    await Promise.race([
+      new Promise((res, rej) => {
+        timer = setTimeout(() => {
+          timer = null
+          rej(`timeout of ${timeout} exceeded when fetching ${page}`)
+        }, timeout)
+      }),
+      new Promise((res, rej) => {
+        dom.window.document.addEventListener("DOMContentLoaded", (evt) => {
+          if (timer) {
+            clearTimeout(timer)
+            res()
+          } else {
+            rej('timeout')
+          }
+        })
+      })
+    ])
+    return { dom, path, url, document: dom.window.document }
+
+    function getDom (page) {
+      // let url = PROTOCOL + '//' + HOST + ':' + PORT + WEBROOT + page
+      return new JSDOM(Fs.readFileSync(path, 'utf8'), {
+        url: url,
+        runScripts: "dangerously",
+        resources: "usable",
+        // resources: new ChattyResourceLoader(),
+      })
+    }
+  }
+
+  // strip hash off URL
+  static noHash (elt) {
+    const u = new URL(elt.href)
+    u.hash = ''
+    return u.href
+  }
+}
+
+// Handy logging resource loader for JSDOM
 class ChattyResourceLoader extends jsdom.ResourceLoader {
   constructor (document) {
     super(Object.assign({}, { userAgent: 'sphinx-to-tr' }, document))
@@ -22,6 +100,7 @@ class ChattyResourceLoader extends jsdom.ResourceLoader {
   }
 }
 
+// Map of arrays (keeps track of total members)
 class ArrayMap extends Map {
   total = 0
 
@@ -43,73 +122,5 @@ class ArrayMap extends Map {
   }
 }
 
-async function index (page) {
-  try {
-    const { dom, document, url } = await loadPage(page, 100)
-    const dir = new URL('..', url).href
-    if (!('$' in dom.window))
-      throw new Error('jQuery failed to load')
-    const find =
-          // (selectors, from) => (from ? dom.window.$(from).find(selectors) : dom.window.$(selectors)).get() // jQuery
-          (selectors, from) => [...(from || document).querySelectorAll(selectors)] // DOM
-    const urlStrToElements =
-          find('a')
-          .map( (elt) => [noHash(elt), elt] )
-          .filter( ([urlStr, elt]) => [urlStr.startsWith(dir), elt] )
-          .map( ([urlStr, elt]) => [urlStr.substr(dir.length), elt] )
-          .reduce( (acc, [urlStr, elt]) => acc.set(urlStr, elt), new ArrayMap())
-    urlStrToElements.delete('')
-    console.log(`${page} has ${urlStrToElements.total} references to ${urlStrToElements.size} descendants of ${dir}`)
-    
-    console.log(('li', find('[role=navigation] ul')[0]))
-    // document.documentElement.outerHTML
-  } catch (e) {
-    console.warn('caught:', e)
-  }
-}
-
-async function loadPage (page, timeout) {
-  const path = Path.join(__dirname, FSROOT, page) // paths relative to repo root
-  let url = new URL('file://' + path)
-  let dom = getDom(page)
-  // dom.window.navigator.appVersion = dom.window.navigator.userAgent
-  let timer = null;
-  await Promise.race([
-    new Promise((res, rej) => {
-      timer = setTimeout(() => {
-        timer = null
-        rej(`timeout of ${timeout} exceeded when fetching ${page}`)
-      }, timeout)
-    }),
-    new Promise((res, rej) => {
-      dom.window.document.addEventListener("DOMContentLoaded", (evt) => {
-        if (timer) {
-          clearTimeout(timer)
-          res()
-        } else {
-          rej('timeout')
-        }
-      })
-    })
-  ])
-  return { dom, path, url, document: dom.window.document }
-
-  function getDom (page) {
-    // let url = PROTOCOL + '//' + HOST + ':' + PORT + WEBROOT + page
-    return new JSDOM(Fs.readFileSync(path, 'utf8'), {
-      url: url,
-      runScripts: "dangerously",
-      resources: "usable",
-      // resources: new ChattyResourceLoader(),
-    })
-  }
-}
-
-function noHash (elt) {
-  const u = new URL(elt.href)
-  u.hash = ''
-  return u.href
-}
-
-index('index.html')
+new SphinxToTr(process.argv[2]).index()
 
