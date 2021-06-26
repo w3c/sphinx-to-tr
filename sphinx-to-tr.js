74378b01ba9ceaa797bbec6ca16c645179792d9f
Eric Prud'hommeaux <eric+github@w3.org>
2021-06-26 09:40:25 +0200

    ~ role=navigation

--- sphinx-to-tr.js
+++ sphinx-to-tr.js
@@ -22,26 +22,47 @@ class ChattyResourceLoader extends jsdom.ResourceLoader {
   }
 }
 
+class ArrayMap extends Map {
+  total = 0
+
+  set (key, value) {
+    ++this.total
+    if (this.has(key)) {
+      this.get(key).push(value)
+      return this
+    } else {
+      return super.set(key, [value])
+    }
+  }
+
+  delete (key) {
+    if (!this.has(key))
+      return false
+    this.total -= this.get(key).length
+    return super.delete(key)
+  }
+}
+
 async function index (page) {
   try {
-    const { dom, url } = await loadPage(page, 100)
+    const { dom, document, url } = await loadPage(page, 100)
     const dir = new URL('..', url).href
     if (!('$' in dom.window))
       throw new Error('jQuery failed to load')
     const find =
-          (selectors) => dom.window.$(selectors).get() // jQuery
-          // (selectors) => [...dom.window.document.querySelectorAll(selectors)] // DOM
-    const urlStrs =
+          // (selectors, from) => (from ? dom.window.$(from).find(selectors) : dom.window.$(selectors)).get() // jQuery
+          (selectors, from) => [...(from || document).querySelectorAll(selectors)] // DOM
+    const urlStrToElements =
           find('a')
-          .map( (elt) => noHash(elt) )
-          .filter( (urlStr) => urlStr.startsWith(dir) )
-          .map( (urlStr) => urlStr.substr(dir.length) )
-          // .sort()
-          // .filter( (urlStr, index, a) => index === a.indexOf(urlStr) )
-          .reduce( (acc, urlStr) => acc.set(urlStr, 1), new Map())
-    console.log(`${urlStrs.length} descendants of ${dir}`)
+          .map( (elt) => [noHash(elt), elt] )
+          .filter( ([urlStr, elt]) => [urlStr.startsWith(dir), elt] )
+          .map( ([urlStr, elt]) => [urlStr.substr(dir.length), elt] )
+          .reduce( (acc, [urlStr, elt]) => acc.set(urlStr, elt), new ArrayMap())
+    urlStrToElements.delete('')
+    console.log(`${page} has ${urlStrToElements.total} references to ${urlStrToElements.size} descendants of ${dir}`)
     
-    console.log(urlStrs)
+    console.log(('li', find('[role=navigation] ul')[0]))
+    // document.documentElement.outerHTML
   } catch (e) {
     console.warn('caught:', e)
   }
@@ -71,7 +92,7 @@ async function loadPage (page, timeout) {
       })
     })
   ])
-  return { dom, path, url }
+  return { dom, path, url, document: dom.window.document }
 
   function getDom (page) {
     // let url = PROTOCOL + '//' + HOST + ':' + PORT + WEBROOT + page
